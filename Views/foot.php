cc3d3c54ab3cdf76ce51d7f410cab72947f30693
frenchcode59 <denisderoullers@gmail.com>
2023-07-04 13:17:07 +0200

    fix sidebar and logo

--- Views/foot.php
+++ Views/foot.php
@@ -10,11 +10,11 @@
     <img src="/bonnefete/Views/BONNE-removebg-preview.png" alt="" style="margin-left: 15%; width: 12%;">
       <div class="footer-right" style="position: absolute;" >
         <div class="logo" style="margin-left: 1500px; margin-top:-100px" >
-        <a href="https://www.facebook.com/?stype=lo&jlou=AfeGcmb43eNnrlPXNY6mvArGFNNAdDr0Rhg46ZSUNtDuzcOq0cNZBO5ByNS-gq-FwlbYWFDK09zvraDYhPYn96DFctervtnXplJZPtxjirjZYA&smuh=8374&lh=Ac-xa0jdB37qq-1582g"><i class="fa fa-facebook"><img src="/bonnefete/Views/Facebook_logo_(square).png" alt="" style="width: 50px;" ></a>
-        <a href="https://twitter.com/i/flow/login?redirect_after_login=%2F%3Flang%3Dfr"><i class="fa fa-twitter"><img src="/bonnefete/Views/Logo_of_Twitter.svg-removebg-preview.png" alt="" style="width: 50px;" ></a>
-        <a href="https://www.linkedin.com/authwall?trk=qf&original_referer=https://www.linkedin.com/feed/&sessionRedirect=https%3A%2F%2Fwww.linkedin.com%2Fhome"><i class="fa fa-linkedin"><img src="/bonnefete/Views/LinkedIn_logo_initials.png" alt="" style="width: 50px;" ></a>
-        <a href="https://www.instagram.com/explore/tags/accueil/"><i class="fa fa-instagram"><img src="/bonnefete/Views/instagram-couleur-removebg-preview.png" alt="" style="width: 50px;" ></a>
-        <a href="https://www.snapchat.com/fr-FR"><i class="fa fa-snapchat"><img src="/bonnefete/Views/20190808214526_Logo-Snapchat-removebg-preview.png" alt="" style="width: 50px;" ></a>
+        <a href="https://www.facebook.com/?stype=lo&jlou=AfeGcmb43eNnrlPXNY6mvArGFNNAdDr0Rhg46ZSUNtDuzcOq0cNZBO5ByNS-gq-FwlbYWFDK09zvraDYhPYn96DFctervtnXplJZPtxjirjZYA&smuh=8374&lh=Ac-xa0jdB37qq-1582g"><i class="fa fa-facebook"><img src="/bonnefete/Views/Facebook_logo_(square).png" alt="" style="width: 5%;" ></a>
+        <a href="https://twitter.com/i/flow/login?redirect_after_login=%2F%3Flang%3Dfr"><i class="fa fa-twitter"><img src="/bonnefete/Views/Logo_of_Twitter.svg-removebg-preview.png" alt="" style="width: 5%;" ></a>
+        <a href="https://www.linkedin.com/authwall?trk=qf&original_referer=https://www.linkedin.com/feed/&sessionRedirect=https%3A%2F%2Fwww.linkedin.com%2Fhome"><i class="fa fa-linkedin"><img src="/bonnefete/Views/LinkedIn_logo_initials.png" alt="" style="width: 5%;" ></a>
+        <a href="https://www.instagram.com/explore/tags/accueil/"><i class="fa fa-instagram"><img src="/bonnefete/Views/instagram-couleur-removebg-preview.png" alt="" style="width: 5%;" ></a>
+        <a href="https://www.snapchat.com/fr-FR"><i class="fa fa-snapchat"><img src="/bonnefete/Views/20190808214526_Logo-Snapchat-removebg-preview.png" alt="" style="width: 5%;" ></a>
       </div>
 </div>
       <div class="footer-left">
