cb73ad461b989d7b927db3c7c8cf79b7081e8e59
frenchcode59 <denisderoullers@gmail.com>
2023-07-04 11:39:02 +0200

    style page login for sidebar

--- Views/user/login.php
+++ Views/user/login.php
@@ -1,6 +1,6 @@
 <?php require_once "Views/head.php"; ?>
 
-<form action="../user/login" method="post">
+<form action="../user/login" method="post" style="margin-bottom: 16%;">
     <label for="mail_user">Email/utilisateur</label>
     <input type="email" name="mail_user" id="mail_user" required>
     <label for="password_user">Mot de Passe</label>
