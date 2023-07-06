a0a05136ad29a3af9424d22d29764465ada1a0a3
frenchcode59 <denisderoullers@gmail.com>
2023-07-06 11:26:56 +0200

    fix : fix and style page register

--- Views/user/register.php
+++ Views/user/register.php
@@ -1,16 +1,24 @@
-<?php
-require_once 'Views/head.php'; ?>
+<?php require_once 'Views/head.php'; ?>
 
-<form action="../user/register" method="post" style="margin-bottom: 10%;">
-    <label for="mail_user">Email</label>
-    <input type="email" name="mail_user" id="mail_user" required>
-    <label for="password_user">Mot de passe</label>
-    <input type="password" name="password_user" id="password_user" required>
-    <label for="pseudo_user">Pseudo</label>
-    <input type="text" name="pseudo_user" id="pseudo_user" required>
-    <p>Vous avez deja un compte? <a href="../user/login"><span>Connectez vous !</span></a></p>
-    <p>En vous inscrivant vous accepter les <a href="../user/rgpd">conditions d'utilisation</a></p>
-    <button type="submit" class="btn btn-success">s'inscrire</button>
-</form>
+<div class="container mt-5 mb-5" style="max-width: 30%">
+    <h1 class="text-center">Inscription</h1>
+    <form action="../user/register" method="post" class="border border-dark shadow-lg p-4 mx-auto">
+        <div class="form-group">
+            <label for="mail_user">Email</label>
+            <input type="email" name="mail_user" id="mail_user" class="form-control" required>
+        </div>
+        <div class="form-group">
+            <label for="password_user">Mot de passe</label>
+            <input type="password" name="password_user" id="password_user" class="form-control" required>
+        </div>
+        <div class="form-group">
+            <label for="pseudo_user">Pseudo</label>
+            <input type="text" name="pseudo_user" id="pseudo_user" class="form-control" required>
+        </div>
+        <p>Vous avez déjà un compte ? <a href="../user/login"><span>Connectez-vous !</span></a></p>
+        <p>En vous inscrivant, vous acceptez les <a href="../user/rgpd">conditions d'utilisation</a></p>
+        <button type="submit" class="btn btn-success">S'inscrire</button>
+    </form>
+</div>
 
 <?php require_once 'Views/foot.php'; ?>
