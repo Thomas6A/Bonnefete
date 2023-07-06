0d0eb3557c18cb1a98b897fb0632bd4d7a4d8b61
frenchcode59 <denisderoullers@gmail.com>
2023-07-06 14:17:35 +0200

    add style page modify profil

--- Views/user/profile.php
+++ Views/user/profile.php
@@ -1,16 +1,31 @@
 <?php require_once 'Views/head.php'; ?>
 
-<form action=<?= "../../user/update/" . $id_user ?> method="post">
-    <div class="form-group">
-        <label for="pseudo_user">Pseudo de l'utilisateur</label>
-        <input type="text" name="pseudo_user" id="pseudo_user" class="form-control" value="<?= $user->getPseudo() ?>">
-        <label for="mail_user">Mail de l'utilisateur</label>
-        <input type="text" name="mail_user" id="mail_user" class="form-control" value="<?= $user->getMail() ?>">
+<div class="container">
+    <h1 class="text-center">Modifier mon profil</h1>
+    <div class="row justify-content-center">
+        <div class="col-12 col-md-6">
+            <form action=<?= "../../user/update/" . $id_user ?> method="post" class="border border-dark shadow p-4 mt-5">
+                <div class="form-group">
+                    <label for="pseudo_user">Pseudo de l'utilisateur</label>
+                    <input type="text" name="pseudo_user" id="pseudo_user" class="form-control" value="<?= $user->getPseudo() ?>">
+                </div>
+                <div class="form-group">
+                    <label for="mail_user">Mail de l'utilisateur</label>
+                    <input type="text" name="mail_user" id="mail_user" class="form-control" value="<?= $user->getMail() ?>">
+                </div>
+                <div class="text-center mt-3">
+                    <button class="btn btn-primary">Mettre à jour</button>
+                    <button class="btn btn-success">
+                        Modifier Mot de passe
+                        <a href="../updatePassword/<?= $id_user ?>" class="mx-3"></a>
+                    </button>
+                </div>
+                <div class="text-center mt-3">
+                    <button class="btn btn-danger">Supprimer profil <a href="../delete/<?= $id_user ?>"></a></button>
+                </div>
+            </form>
+        </div>
     </div>
-    <button class="btn btn-primary"><?= "Mettre à jour" ?></button>
-    <a href="../updatePassword/<?= $id_user ?>">Modifier Mot de passe</a>
-    <a href="../delete/<?= $id_user ?>">Supprimer Profile</a>
-</form>
-
+</div>
 
 <?php require_once 'Views/foot.php'; ?>
